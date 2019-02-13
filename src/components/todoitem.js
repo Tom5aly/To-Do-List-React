56f11f2f7a3c0da3f50a4f0b28abfaa439a813be
Tom5aly <tom.aly@hotmail.com>
2019-02-13 18:35:53 +0000

    checkbox

--- src/components/todoitem.js
+++ src/components/todoitem.js
@@ -5,11 +5,15 @@ export default class todoitem extends Component {
     const { title, handelDeletItem } = this.props;
     return (
       <li className="list-group-item text-capitalize d-flex justify-content-between my-2 shadow-lg  bg-black rounded ">
+        <label>
+          <input type="checkbox" className="checkmark" />
+        </label>
         <h6>{title}</h6>
+
         <div className="tod-icon">
           <span
             onClick={handelDeletItem}
-            className="mx-2 text-denger"
+            className="mx-3 text-denger"
             style={{ cursor: "pointer" }}
           >
             <i className="fas fa-trash" />
